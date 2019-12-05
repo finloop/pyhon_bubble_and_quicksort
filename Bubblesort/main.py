f79fd06dcb74868305945ad4995aa93d48eb4b72
finloop <piotr.krawiec23@gmail.com>
2019-12-05 16:31:21 +0100

    f sprawko

--- Bubblesort/main.py
+++ Bubblesort/main.py
@@ -2,9 +2,7 @@ def bubbleSort(sorted_list):
     for passnum in range(len(sorted_list)-1,0,-1):
         for i in range(passnum):
             if sorted_list[i]>sorted_list[i+1]:
-                temp = sorted_list[i]
-                sorted_list[i] = sorted_list[i+1]
-                sorted_list[i+1] = temp
+                sorted_list[i], sorted_list[i+1] = sorted_list[i+1], sorted_list[i]
 
 numeric_list = [14,46,57,21,70,2,101,55,27,223]
 sorted_numeric_list = [2,14,21,27,46,55,57,70,101,223]
